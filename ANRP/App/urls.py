82853077f19e235a8e711af666ed246a20749cf6
Mohamed <elhaddadm800@gmail.com>
2024-05-26 11:12:55 +0100

    new

--- ANRP/App/urls.py
+++ ANRP/App/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import image_upload
 
 urlpatterns = [
-    path('upload/', image_upload, name='image_upload'),
+    path('', image_upload, name='image_upload'),
 ]
--- ANRP/plateDetection/urls.py
+++ ANRP/plateDetection/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('App/', include('App.urls')),
+    path('', include('App.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
